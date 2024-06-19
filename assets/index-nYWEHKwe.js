48fd905c05ad4b10198d24b0d1ce7ad3ccdd36fc
Prince <princeowenolive@gmail.com>
2024-06-19 04:15:32 +0100

    Implemented Factory Agnostic Listing

--- assets/index-nYWEHKwe.js
+++ assets/index-nYWEHKwe.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-DP1euugV.js";import{aL as Ur}from"./index-JgB9NUTH.js";/**
+import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-DEtAy1iM.js";import{aL as Ur}from"./index-CjbQOMLF.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
