63f339184ffc0a655a521c5230da8b76b8f00f77
Prince <princeowenolive@gmail.com>
2024-06-20 23:16:46 +0100

    Switched to Polygon Mainnet

--- assets/index-DNpsq7Pn.js
+++ assets/index-DNpsq7Pn.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-DEtAy1iM.js";import{aL as Ur}from"./index-CjbQOMLF.js";/**
+import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-B78NNEub.js";import{aL as Ur}from"./index-D3V9yZZx.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
